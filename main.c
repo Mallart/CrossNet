6a951f272e24349ee567f73c2d37f85643bfa256
Mallart <matheo.allart09@gmail.com>
2025-04-13 15:46:38 +0200

    main test case updated for client and server to communicate

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include "./server/server.h"
 
 // This runs a basic server for 5 sec.
-void server(void)
+int server(void)
 {
 	server_setup_log(stdout, stderr);
 	CN_SOCKET _server =
@@ -17,10 +17,21 @@ void server(void)
 	server_listen(&_server, 0xff);
 	thrd_sleep(&(struct timespec) { .tv_sec = 15 }, 0);
 	server_shutdown(&_server, CN_SERVER_PROHIBIT_SEND);
+	return 0;
+}
+
+// This runs a basic client that reaches to the examples server.
+int client(void)
+{
+	return 0;
 }
 
 int main(void)
 {
-	server();
+	thrd_t tserver, tclient;
+	thrd_create(&tserver, &server, (void*)0);
+	thrd_create(&tclient, &client, (void*)0);
+	thrd_join(tserver, 0);
+	thrd_join(tclient, 0);
 	return 0;
 }
